36f8344cfdb0e03429ef649ebfdc0da9e547826d
JrubioCode <javii.central.7@gmail.com>
2024-11-06 18:56:50 +0100

    Tragaperras funcional

---
+++ tragaperras.js
@@ -0,0 +1,163 @@
+// FUNCIONALIDADES HEADER
+
+// Gestión del saldo
+var saldo = 0;
+
+const ingresar = document.getElementById("ingresar");
+const retirar = document.getElementById("retirar");
+const botonMonedas = document.getElementById("botonMonedas");
+const mostrarSaldo = document.getElementById("saldo");
+
+function actualizarSaldo() {
+  mostrarSaldo.textContent = "SALDO ACTUAL: ".concat(saldo);
+}
+
+botonMonedas.addEventListener("click", function () {
+  const puntosIngresar = parseInt(ingresar.value) || 0;
+  const puntosRetirar = parseInt(retirar.value) || 0;
+
+  if (puntosIngresar > 0 && puntosRetirar === 0) {
+    saldo += puntosIngresar;
+  } else if (puntosRetirar > 0 && puntosIngresar === 0) {
+    saldo -= puntosRetirar;
+  } else {
+    window.alert("Por favor, llena solo un campo para añadir o retirar puntos.");
+  }
+
+  ingresar.value = "";
+  retirar.value = "";
+
+  actualizarSaldo();
+});
+
+// Reloj
+function actualizarReloj() {
+  const ahora = new Date();
+  const reloj = ahora.toLocaleTimeString();
+  document.getElementById("reloj").textContent = reloj;
+}
+setInterval(actualizarReloj, 1000);
+actualizarReloj();
+
+// Modal ajustes
+function modalAjustes() {
+  const modal = document.getElementById("modal-ajustes");
+  if (modal.style.display === "none" || modal.style.display === "") {
+    modal.style.display = "flex";
+    document.addEventListener("click", cerrarModal);
+  } else {
+    modal.style.display = "none";
+    document.removeEventListener("click", cerrarModal);
+  }
+}
+
+// Cerrar modal
+function cerrarModal(event) {
+  const modal = document.getElementById("modal-ajustes");
+  const iconoAjustes = document.querySelector(".icono-ajustes");
+
+  if (!modal.contains(event.target) && event.target !== iconoAjustes) {
+    modal.style.display = "none";
+    document.removeEventListener("click", cerrarModal);
+  }
+}
+
+// Audio general
+var audio = new Audio("./audios/audio-principal.mp3");
+audio.loop = true;
+
+var sonando = false;
+var volumenImg = document.getElementById("volumen");
+
+volumenImg.addEventListener("click", function() {
+  if (!sonando) {
+    audio.play();
+    sonando = true;
+  } else {
+    if (audio.volume > 0) {
+      audio.volume = 0;
+      volumenImg.src="./assets/ajustes/sin-volumen.png";
+    } else {
+      audio.volume = 1;
+      volumenImg.src="./assets/ajustes/volumen.png";
+    }
+  }
+});
+
+// FUNCIONALIDADES TRAGAPERRAS
+
+// Símbolos tragaperras
+const simbolos = [
+  "./assets/tragaperras/cavernicola.png",
+  "./assets/tragaperras/fuego.png",
+  "./assets/tragaperras/pollo.png",
+  "./assets/tragaperras/mamut.png",
+  "./assets/tragaperras/grupoCavernicolas.png",
+];
+
+// Evento al pulsar espacio
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    iniciarGiro();
+  }
+});
+
+// Palanca
+var palanca = document.getElementById("palanca");
+palanca.addEventListener("click", function () {
+  cambiarPalanca();
+});
+
+function cambiarPalanca() {
+  palanca.src = "./assets/tragaperras/palanca_abajo.png";
+
+  var intervalo = setInterval(function () {
+    palanca.src = "./assets/tragaperras/palanca_arriba.png";
+    clearInterval(intervalo);
+  }, 200);
+}
+
+// Evento al hacer clic en la palanca
+palanca.addEventListener("click", iniciarGiro);
+
+// Iniciar el giro de los carriles
+function iniciarGiro() {
+  giroCarriles("carril1", 2000);
+  giroCarriles("carril2", 4000);
+  giroCarriles("carril3", 6000);
+}
+
+// Función para girar cada carril
+function giroCarriles(carrilId, duracion) {
+  const carril = document.getElementById(carrilId);
+  var tiempoInicio = null;
+
+  function animarGiro(timestamp) {
+    if (!tiempoInicio) tiempoInicio = timestamp;
+    const progreso = timestamp - tiempoInicio;
+
+    const simboloAleatorio = simbolos[Math.floor(Math.random() * simbolos.length)];
+    const imagen = document.createElement("img");
+    imagen.src = simboloAleatorio;
+    imagen.classList.add("simbolo_Imagen");
+    carril.appendChild(imagen);
+
+    if (carril.childNodes.length > 5) {
+      carril.removeChild(carril.firstChild);
+    }
+
+    if (progreso < duracion) {
+      requestAnimationFrame(animarGiro);
+    } else {
+      detenerGiro(carril);
+    }
+  }
+
+  requestAnimationFrame(animarGiro);
+}
+
+// Función para detener el giro y mostrar el símbolo final
+function detenerGiro(carril) {
+  const simboloFinal = simbolos[Math.floor(Math.random() * simbolos.length)];
+  carril.innerHTML = `<img src="${simboloFinal}" alt="símbolo" class="simbolo_Imagen">`;
+}
